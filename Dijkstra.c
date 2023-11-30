3fc4a019a745bf26927e12f731bb415a2085885e
Nikolexion <nfalc2003@gmail.com>
2023-11-29 20:01:52 -0400

    a

---
+++ Dijkstra.c
@@ -0,0 +1,57 @@
+void dijkstra(int matrizAdyacencia[112][112], int inicio, int final) {
+    int distancias[112], anteriores[112], visto[112], camino[112], actual = inicio;
+
+    for (int i = 0; i < 112; i++) {
+        distancias[i] = 9999;           //Dejamos las distancias en un numero alto al inicio, ya que no tenemos un "infinito" 
+        anteriores[i] = -1;             //Dejamos el vector de anteriores en -1 para mostrar que se aun no empieza 
+        visto[i] = 0;               //Dejamos el vector en 0 ya que ningun vertice ha sido visitado
+    }
+
+    visto[actual] = 1;
+    distancias[actual] = 0;
+
+    while (!visto[final]) {
+        int distanciaMinima = 9999;
+        int vertice = 0;
+
+        for (int i = 0; i < 112; i++) {
+            int distancia = distancias[actual] + matrizAdyacencia[actual][i];
+            if (distancia < distancias[i] && !visto[i]) {
+                distancias[i] = distancia;
+                anteriores[i] = actual;
+            }
+            if (distanciaMinima > distancias[i] && !visto[i]) {
+                distanciaMinima = distancias[i];
+                vertice = i;
+            }
+        }
+      actual = vertice;
+        visto[actual] = 1;
+    }
+
+    actual = final;
+
+    int longitudCamino = 0;
+    while (actual != inicio) {
+        camino[longitudCamino++] = actual;
+        actual = anteriores[actual];
+    }
+    camino[longitudCamino++] = actual;
+
+    for (int i = 0; i < longitudCamino / 2; i++) {
+        int aux = camino[i];
+        camino[i] = camino[longitudCamino - i - 1];
+        camino[longitudCamino - i - 1] = aux;
+    }
+
+    printf("Camino desde %d hasta %d: ", inicio, final);
+    for (int i = 0; i < longitudCamino; i++) {
+        if (i == longitudCamino - 1) {
+            printf("%d\n", camino[i]);
+        } else {
+            printf("%d ", camino[i]);
+        }
+    }
+
+    printf("Distancia desde %d hasta %d: %d\n", inicio, final, distancias[final]);
+}
--- Tarea_Computacional.c
+++ Tarea_Computacional.c
@@ -3,10 +3,17 @@
 
 #define NUM_VERTICES 112
 #define MAX_CALLE_LEN 50
-#define FILENAME "nombresIntersecciones.txt"
+#define FILENAME "nombreIntersecciones.txt"
 
 // Definición de la matriz de adyacencia
 int matrizAdyacencia[NUM_VERTICES][NUM_VERTICES];
+
+typedef struct {
+    int vertice;
+    char calle[MAX_CALLE_LEN];
+} Nodo;
+
+Nodo mapaCalles[NUM_VERTICES];
 
 // Estructura para almacenar la correspondencia entre vértices y calles
 
@@ -15,29 +22,36 @@ int main() {
     char inputString[] = "Orompello 314 Chacabuco 201";
 
     // Variables para almacenar las palabras separadas
-    char palabra1[50], palabra2[50], palabra3[50], palabra4[50];
+    char calle1[50], numero1[50], calle2[50], numero2[50];
 
     // Llamar a la función para separar las palabras
-    separarPalabras(inputString, palabra1, palabra2, palabra3, palabra4);
+    //separarPalabras(inputString, calle1, numero1, calle2, numero2);
 
     // Mostrar las palabras por separado
-    //printf("Palabra 1: %s\n", palabra1);
-    //printf("Palabra 2: %s\n", palabra2);
-    //printf("Palabra 3: %s\n", palabra3);
-    //printf("Palabra 4: %s\n", palabra4);
+    //printf("Palabra 1: %s\n", calle1);
+    //printf("Palabra 2: %s\n", numero1);
+    //printf("Palabra 3: %s\n", calle1);
+    //printf("Palabra 4: %s\n", numero2);
 
-        const char* input = "Rengo";
-        const char* input2 = "Los_Carrera";
-    int numeroPalabra = obtenerCalleX(palabra1);
-    //int numeroPalabra2 = obtenerCalleX(input2);
-    if (numeroPalabra != -1)
+    //const char* input = "Rengo";
+    //const char* input2 = "Los_Carrera";
+
+    //char* numeroPalabra = obtenerCalleX(calle1);
+    //char* numeroPalabra2 = obtenerCalleX(input2);
+    leerVerticesDesdeArchivo(FILENAME);
+    for (int i = 0; i < 6; i++)
     {
-        printf("La calle '%s' es horizontal y su nro es '%d'", palabra1,numeroPalabra);
-    } else {
-        numeroPalabra = obtenerCalleY(palabra1);
-        printf("La calle '%s' es vertical y su nro es '%d'", palabra1,numeroPalabra);
-
+        printf("%s", mapaCalles[i].calle);
     }
+    
+//    if (numeroPalabra != -1)
+//    {
+//        printf("La calle '%s' es horizontal y su nro es '%d'", calle1,numeroPalabra);
+//    } else {
+//        numeroPalabra = obtenerCalleY(calle1);
+//        printf("La calle '%s' es vertical y su nro es '%d'", calle1,numeroPalabra);
+//
+//    }
     
 
     /*if (numeroPalabra != -1) {
@@ -52,7 +66,7 @@ int main() {
 
 
 
-void separarPalabras(const char* inputString, char* palabra1, char* palabra2, char* palabra3, char* palabra4) {
+void separarPalabras(const char* inputString, char* calle1, char* numero1, char* calle2, char* numero2) {
     char copiaInput[100]; // Asumiendo un tamaño máximo para la cadena de entrada
 
     // Copiar la cadena de entrada para evitar modificar la original
@@ -61,13 +75,23 @@ void separarPalabras(const char* inputString, char* palabra1, char* palabra2, ch
     char* token = strtok(copiaInput, " "); // Dividir la cadena en palabras usando el espacio como delimitador
 
     // Copiar cada palabra en las variables correspondientes
-    strcpy(palabra1, token);
+    strcpy(calle1, token);
     token = strtok(NULL, " ");
-    strcpy(palabra2, token);
+    strcpy(numero1, token);
     token = strtok(NULL, " ");
-    strcpy(palabra3, token);
+    strcpy(calle2, token);
     token = strtok(NULL, " ");
-    strcpy(palabra4, token);
+    strcpy(numero2, token);
+}
+
+int obtenerCalle(const char* palabra, int *vertical){
+    if (obtenerCalleX(palabra) != -1){
+        vertical = 1;
+        return obtenerCalleX(palabra);
+    } else{
+        vertical = 0;
+        return obtenerCalleY(palabra);
+    }
 }
 
 int obtenerCalleX(const char* palabra) {
@@ -110,15 +134,10 @@ int obtenerCalleY(const char* palabra) {
 }
 
 
-typedef struct {
-    int vertice;
-    char calle[MAX_CALLE_LEN];
-} Correspondencia;
 
-Correspondencia mapaCalles[NUM_VERTICES];
 
 // Función para convertir un vértice a una calle
-char* obtenerCalle(int vertice) {
+char* nombreVertice(int vertice) {
     leerVerticesDesdeArchivo(FILENAME);
     // Busca el vértice en el mapa
     for (int i = 0; i < NUM_VERTICES; i++) {
